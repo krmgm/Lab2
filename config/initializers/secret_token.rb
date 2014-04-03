593e3377522eecf9f93d02be390b0889272ec91d
krmgm <krmgm@galileo.edu>
2014-04-02 21:04:41 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lab2::Application.config.secret_key_base = '31916e995367a91dd68421fe79d14c4cc388f0866699ffe501bb92bd2f017449a263b7fcbd18d7e2936d1962edb842c26c27ac57900eeea21508b0dbb7a60b2d'
